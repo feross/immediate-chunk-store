76ea1b2299be4a08b45ca24b686c900129a8819f
Feross Aboukhadijeh <feross@feross.org>
2018-08-03 23:57:59 -0700

    test: ESnext

--- test.js
+++ test.js
@@ -1,24 +1,24 @@
-var abstractTests = require('abstract-chunk-store/tests')
-var ImmediateChunkStore = require('./')
-var FSChunkStore = require('fs-chunk-store')
-var MemoryChunkStore = require('memory-chunk-store')
-var test = require('tape')
+const abstractTests = require('abstract-chunk-store/tests')
+const ImmediateChunkStore = require('./')
+const FSChunkStore = require('fs-chunk-store')
+const MemoryChunkStore = require('memory-chunk-store')
+const test = require('tape')
 
-abstractTests(test, function (chunkLength) {
+abstractTests(test, chunkLength => {
   return new ImmediateChunkStore(new MemoryChunkStore(chunkLength))
 })
 
-abstractTests(test, function (chunkLength) {
+abstractTests(test, chunkLength => {
   return new ImmediateChunkStore(new FSChunkStore(chunkLength))
 })
 
-test('put then immediate get', function (t) {
-  var store = new ImmediateChunkStore(new FSChunkStore(10))
+test('put then immediate get', t => {
+  const store = new ImmediateChunkStore(new FSChunkStore(10))
 
   store.put(0, Buffer.from('0123456789'), onPut)
 
   // And now, get the same chunk out BEFORE the put is complete
-  store.get(0, function (err, data) {
+  store.get(0, (err, data) => {
     t.error(err)
     t.deepEqual(data, Buffer.from('0123456789'))
     didGet1 = true
@@ -29,7 +29,7 @@ test('put then immediate get', function (t) {
     t.error(err)
 
     // Getting after put should still work
-    store.get(0, function (err, data) {
+    store.get(0, (err, data) => {
       t.error(err)
       t.deepEqual(data, Buffer.from('0123456789'))
       didGet2 = true
@@ -37,11 +37,11 @@ test('put then immediate get', function (t) {
     })
   }
 
-  var didGet1 = false
-  var didGet2 = false
+  let didGet1 = false
+  let didGet2 = false
   function maybeDone () {
     if (didGet1 && didGet2) {
-      store.destroy(function (err) {
+      store.destroy(err => {
         t.error(err)
         t.end()
       })
